15dce95ac41c50ea7214befdd04ee14c965e1cb2
Sagar-Roy <raysagar077@gmail.com>
2026-02-06 01:57:47 +0530

    remove

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./index.css";
-import ScrollToTop from "./ScrollToTop.jsx";
-import LanguageProvider from "./LanguageContext.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    <LanguageProvider>
-      <ScrollToTop />
-      <App />
-    </LanguageProvider>
-  </HashRouter>,
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
